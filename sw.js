f759e28894b537966e119c9752f68680cfc47392
gmasia217 <jenny02.hong@gmail.com>
2026-06-08 12:06:58 +0900

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'oz-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './oz_logo.png',
+  './sidereus_icon.jpg'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) return response;
+        return fetch(event.request);
+      })
+  );
+});
